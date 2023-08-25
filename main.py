aa25c6e7c8028da0fcedf91dd4870fcbc1b2a4da
alphamouhamadou <alphamouhamadoudiop@gmail.com>
2023-08-25 20:16:20 +0000

    modif programme

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
+import subprocess
 
 def create_project_structure():
+    subprocess.run(["git", "init"])  # Initialisation du dépôt Git
+
     directories = [
         "data/cleaned",
         "data/raw",
@@ -13,17 +16,24 @@ def create_project_structure():
 
     for directory in directories:
         os.makedirs(directory, exist_ok=True)
-        create_requirement_file(directory)
+        create_file(directory, "placeholder.txt", "Ce fichier est un espace réservé.")
+        subprocess.run(["git", "add", directory])
+        subprocess.run(["git", "commit", "-m", f"Added {directory}"])
 
-    create_file("notebooks/main.ipynb", "# main")
-    create_file("src/utils.py", "# Util")
+    create_file("notebooks", "main.ipynb", "# Notebook Content")
+    subprocess.run(["git", "ajout", "notebooks"])
+    subprocess.run(["git", "commit", "-m", "Ajout main notebook"])
 
-def create_requirement_file(directory):
-    file_path = os.path.join(directory, "requirement.txt")
-    with open(file_path, "w") as file:
-        file.write("C  fichier contenant la liste des dépendances du projets")
+    create_file("src", "utils.py", "# Util")
+    create_file("src", "main.py", "# Main Code")
+    create_file("data/cleaned", "utils.py", "# Cleaned Utility Functions")
+    create_file("data/raw", "utils.py", "# Raw Utility Functions")
+    create_file("", "requirements.txt", "numpy\npandas\nmatplotlib")
+    subprocess.run(["git", "ajout", "src", "data", "requirements.txt"])
+    subprocess.run(["git", "commit", "-m", "ajout requirements"])
 
-def create_file(file_path, content):
+def create_file(directory, file_name, content):
+    file_path = os.path.join(directory, file_name)
     with open(file_path, "w") as file:
         file.write(content)
 
